2f6cf8c08b3e0cbf940c9ec3343b04c1e94967e1
stephanobenny03 <stephanobenny03gmail.com>
2026-07-13 13:01:39 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddFlight/>}/>
+      <Route path='/add' element={<AddFlight/>}/>
+      <Route path='/view' element={<ViewFlight/>}/>
+    </Routes>
+    
+    </BrowserRouter>
      
     </>
   )
